ffb6b62beb190278534cae90e84170d98da5512c
Aya Freya Ahmed <ada@chronovore.dev>
2026-03-14 15:17:03 +0530

    pfs: less spaghetti

--- Akizuki/PackageFileSystem/Package.cs
+++ Akizuki/PackageFileSystem/Package.cs
@@ -37,51 +37,17 @@ public abstract class Package : BigWorldFile {
 				switch (resourceHeader.CompressionType) {
 					case PackageCompressionType.None: throw new UnreachableException();
 					case PackageCompressionType.Deflate when resourceHeader.CompressionSystem is PackageCompressionSystem.Block: {
-						int n;
 						if (compressedSpan.Length >= 8 && MemoryMarshal.Read<uint>(compressedSpan) == 0) {
-							var blockInfoCount = MemoryMarshal.Read<int>(compressedSpan[..4]);
-							var blockBytes = blockInfoCount << 2;
-							var blockInfos = MemoryMarshal.Cast<byte, PackageBlock>(compressedSpan[8..blockBytes]);
-							var remainingSize = data.Length;
-							var totalSize = remainingSize;
-							var offset = 8 + blockBytes;
-							const int BLOCK_SIZE = 0x10000;
-							foreach (var blockInfo in blockInfos) {
-								offset += blockInfo.Size;
-								var start = int.CreateChecked(totalSize - remainingSize);
-								var size = Math.Min(remainingSize, BLOCK_SIZE);
-								var end = int.CreateChecked(start + Math.Min(remainingSize, BLOCK_SIZE));
-								Debug.Assert(blockInfo.IsCompressed is 0 or 1);
-								n = CompressionHelper.Decompress(blockInfo.IsCompressed == 0 ? CompressionType.None : CompressionType.Deflate, compressedMemory[offset..(offset + blockInfo.Size)], dataMemory[start..end]);
-								Debug.Assert(n == size);
-								offset += blockInfo.Size;
-								remainingSize -= size;
-							}
-
+							DecompressBlocks<TPointer>(CompressionType.Deflate, compressedSpan, data, compressedMemory, dataMemory);
 							break;
 						}
 
-						n = CompressionHelper.Decompress(CompressionType.Deflate, compressedMemory, dataMemory);
+						var n = CompressionHelper.Decompress(CompressionType.Deflate, compressedMemory, dataMemory);
 						Debug.Assert(n == data.Length);
 						break;
 					}
 					case >= PackageCompressionType.OodleKraken and <= PackageCompressionType.OodleHydra when resourceHeader.CompressionSystem is PackageCompressionSystem.TileStream: {
-						var streamHeader = MemoryMarshal.Read<TTileStream>(compressedMemory.Span);
-						var remainingSize = long.CreateChecked(streamHeader.Size);
-						var totalSize = remainingSize;
-						var offset = int.CreateChecked(streamHeader.DataOffset) + 8;
-						var blocks = MemoryMarshal.Cast<byte, int>(compressedMemory.Span[Unsafe.SizeOf<TTileStream>()..])[..streamHeader.BlockCount];
-						Debug.Assert(streamHeader.Size == resourceHeader.Size);
-						foreach (var block in blocks) {
-							var start = int.CreateChecked(totalSize - remainingSize);
-							var size = Math.Min(remainingSize, streamHeader.BlockSize);
-							var end = int.CreateChecked(start + Math.Min(remainingSize, streamHeader.BlockSize));
-							var n = CompressionHelper.Decompress(CompressionType.Oodle, compressedMemory[offset..(offset + block)], dataMemory[start..end]);
-							Debug.Assert(n == size);
-							offset += block;
-							remainingSize -= size;
-						}
-
+						DecompressTileStream<TPointer, TResource, TTileStream>(CompressionType.Oodle, resourceHeader, compressedMemory, dataMemory);
 						break;
 					}
 					default: throw new NotSupportedException($"compression type {resourceHeader.CompressionType} with subsystem {resourceHeader.CompressionSystem} is not supported");
@@ -102,5 +68,44 @@ public abstract class Package : BigWorldFile {
 		}
 
 		return data;
+	}
+
+	private static void DecompressBlocks<TPointer>(CompressionType compressionType, Span<byte> compressedSpan, RentedArray<byte> data, Memory<byte> compressedMemory, Memory<byte> dataMemory) where TPointer : INumber<TPointer> {
+		var blockInfoCount = MemoryMarshal.Read<int>(compressedSpan[..4]);
+		var blockBytes = blockInfoCount << 2;
+		var blockInfos = MemoryMarshal.Cast<byte, PackageBlock>(compressedSpan[8..blockBytes]);
+		var remainingSize = data.Length;
+		var totalSize = remainingSize;
+		var offset = 8 + blockBytes;
+		const int BLOCK_SIZE = 0x10000;
+		foreach (var blockInfo in blockInfos) {
+			offset += blockInfo.Size;
+			var start = int.CreateChecked(totalSize - remainingSize);
+			var size = Math.Min(remainingSize, BLOCK_SIZE);
+			var end = int.CreateChecked(start + Math.Min(remainingSize, BLOCK_SIZE));
+			Debug.Assert(blockInfo.IsCompressed is 0 or 1);
+			var n = CompressionHelper.Decompress(blockInfo.IsCompressed == 0 ? CompressionType.None : compressionType, compressedMemory[offset..(offset + blockInfo.Size)], dataMemory[start..end]);
+			Debug.Assert(n == size);
+			offset += blockInfo.Size;
+			remainingSize -= size;
+		}
+	}
+
+	private static void DecompressTileStream<TPointer, TResource, TTileStream>(CompressionType compressionType, TResource resourceHeader, Memory<byte> compressedMemory, Memory<byte> dataMemory) where TPointer : INumber<TPointer> where TResource : IPackageStreamedResource<TPointer> where TTileStream : struct, IPackageTileStream<TPointer> {
+		var streamHeader = MemoryMarshal.Read<TTileStream>(compressedMemory.Span);
+		var remainingSize = long.CreateChecked(streamHeader.Size);
+		var totalSize = remainingSize;
+		var offset = int.CreateChecked(streamHeader.DataOffset) + 8;
+		var blocks = MemoryMarshal.Cast<byte, int>(compressedMemory.Span[Unsafe.SizeOf<TTileStream>()..])[..streamHeader.BlockCount];
+		Debug.Assert(streamHeader.Size == resourceHeader.Size);
+		foreach (var block in blocks) {
+			var start = int.CreateChecked(totalSize - remainingSize);
+			var size = Math.Min(remainingSize, streamHeader.BlockSize);
+			var end = int.CreateChecked(start + Math.Min(remainingSize, streamHeader.BlockSize));
+			var n = CompressionHelper.Decompress(compressionType, compressedMemory[offset..(offset + block)], dataMemory[start..end]);
+			Debug.Assert(n == size);
+			offset += block;
+			remainingSize -= size;
+		}
 	}
 }
